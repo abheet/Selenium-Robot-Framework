c0275e69a306f88c0c32ba97c685a3b25430d47f
Abheet Jamwal <abheet.jamwal@gmail.com>
2020-10-06 16:07:13 +0530

    Updated code in conftest file

--- SeleniumScripts/config.py
+++ SeleniumScripts/config.py
@@ -4,7 +4,7 @@ LOGGER = logging.getLogger(__name__)
 #Which browser need for test
 
 #Possible values :  CHROME/FIREFOX or IE
-WHICH_DRIVER = 'CHROME' 
+WHICH_DRIVER = 'FIREFOX' 
 
 #driver paths
 CHROME_DRIVER = 'BrowserDrivers/Chrome/linux/chromedriver'
--- SeleniumScripts/conftest.py
+++ SeleniumScripts/conftest.py
@@ -1,27 +1,39 @@
 import pytest
 from selenium import webdriver
 import config
+import time
 
-
-#Pytest Fixture get driver path
+# Pytest Fixture get driver path
 @pytest.fixture(scope="class", params=[config.WHICH_DRIVER])
 def browser(request):
-    #Setup details 
+    # Setup details
     driver_instance = ""
     if request.param == 'IE':
-            driver_instance =  webdriver.Ie(executable_path=config.IE_DRIVER)
+        driver_instance = webdriver.Ie(executable_path=config.IE_DRIVER)
     elif request.param == 'FIREFOX':
-        driver_instance = webdriver.Firefox(executable_path=config.FIREFOX_DRIVER)
+        driver_instance = webdriver.Firefox(
+            executable_path=config.FIREFOX_DRIVER)
     elif request.param == 'CHROME':
-        driver_instance = webdriver.Chrome(executable_path=config.CHROME_DRIVER)  
-    
-    driver_instance.maximize_window()    
-    request.cls.driver = driver_instance
-    config.LOGGER.info("Driver inititalized")
-    yield
-    #Teardown steps
+        driver_instance = webdriver.Chrome(
+            executable_path=config.CHROME_DRIVER)
+    driver_instance.maximize_window()
+    driver_instance.get("https://my.symphony.com")
+    time.sleep(5)
+    # Skip test is browser not compatible
+    if driver_instance.current_url == 'https://my.symphony.com/browsers.html':
+        driver_instance.close()
+        driver_instance.quit()
+        pytest.skip("Browser is no compatible with App")
+        config.LOGGER.info(
+            "Browser is not compatible with app. Skipping test..")
+       
+    else:
+        request.cls.driver = driver_instance
+        config.LOGGER.info("Driver inititalized")
+        
+    yield    
+    # Teardown steps
+    driver_instance.close()
     driver_instance.quit()
     config.LOGGER.info("Driver closed")
-
-
-  
+        
